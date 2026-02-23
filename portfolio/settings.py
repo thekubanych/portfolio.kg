d90b5cf3f8d296f193ecd9e9bea705d7234cacf3
thekubanych <kubanychmuhtarov>
2026-02-24 04:07:37 +0600

    Fix settings and deps

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -5,7 +5,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-thekubanych-change-in-prod')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
+ALLOWED_HOSTS = ["portfolio-kg.onrender.com", "localhost", "127.0.0.1"]
+
 
 INSTALLED_APPS = [
     'jazzmin',
